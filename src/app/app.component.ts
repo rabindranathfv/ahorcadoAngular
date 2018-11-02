3564ebe1a110c10d30fbcc8f1b64688fd748708f
Rabindranath <ferreiralox@gmail.com>
2018-11-01 21:36:04 -0300

    add replace rigth letter, add chances to win

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ahorcado';
+  palabra = 'RABINDRANATH';
+  palabraOculta = '';
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  intentos = 0;
+  constructor() {
+    this.palabraOculta = '_ '.repeat( this.palabra.length );
+  }
+
+  comprobar(letra) {
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+    for (let index = 0; index < this.palabra.length; index++) {
+      const element = this.palabra[index];
+      if ( element === letra ) {
+        palabraOcultaArr[index] = letra;
+      }
+
+    }
+    this.palabraOculta = palabraOcultaArr.join(' ');
+  }
 }
